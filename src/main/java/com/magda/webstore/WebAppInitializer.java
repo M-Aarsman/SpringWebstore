e96223f53284818f1fd5d4ad1948628cbb014bc8
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2019-03-10 22:05:44 +0100

    hello world, simple hello website

---
+++ src/main/java/com/magda/webstore/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package com.magda.webstore;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(MvcConfiguration.class);
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.addMapping("/");
+    }
+}
